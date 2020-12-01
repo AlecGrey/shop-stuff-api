be845d963c96660df73e6699360611bc859b8f18
AlecGrey <alecgrey.coding@gmail.com>
2020-12-01 12:59:01 -0800

    generated migrations and model classes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+require 'json'
+require 'pry'
+
+max_taxonomy = 1484
+# items = []
+
+until Item.all.length == 100 do
+
+    data = RestClient.get("https://openapi.etsy.com/v2/listings/active?taxonomy_id=#{rand(1..max_taxonomy)}&api_key=hpe7uoia9bb85qcm5gh459xq")
+    json = JSON.parse(data)
+
+    if json['count'] != 0 
+        etsy_item = json['results'][rand(0...json['results'].length)]
+
+        data = RestClient.get("https://openapi.etsy.com/v2/listings/#{etsy_item['listing_id']}/images?api_key=hpe7uoia9bb85qcm5gh459xq")
+        json = JSON.parse(data)
+
+        item_img_url = json['results'][0]['url_570xN']
+
+        # items << below hash
+        Item.create({
+            item_id: etsy_item['listing_id'],
+            name: etsy_item['title'],
+            description: etsy_item['description'],
+            price: etsy_item['price'].to_f,
+            item_type: etsy_item['taxonomy_path'][0],
+            img_url: item_img_url
+        })
+    end
+end
+
+# items.each do |item|
+#     data = RestClient.get("https://openapi.etsy.com/v2/listings/#{item[:item_id]}/images?api_key=hpe7uoia9bb85qcm5gh459xq")
+#     json = JSON.parse(data)
+#     item[:img_url] = json['results'][0]['url_570xN']
+#     Item.create(item)
+# end
