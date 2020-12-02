4369681f5de3987898adcfa065fa243d1f54828a
AlecGrey <alecgrey.coding@gmail.com>
2020-12-02 12:01:35 -0800

    created search query functionality in items#index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
 
     def index
-        items = Item.all
+        page_number = 1
+
+        if params[:category] || params[:query]
+            items = filter_items(items: Item.all, query: params[:query], category: params[:category])
+        else
+            items = Item.all
+        end
+
+        items = items.slice(10 * (page_number - 1), 10)
         render json: items, except: [:created_at, :updated_at, :description]
+    end
+
+    def categories
+        types = Item.all.map {|i| i.item_type}.uniq.sort
+        render json: {
+            categories: types
+        }
+    end
+
+    private
+
+    def filter_items(items:, query:, category:)
+        output = items
+        if category != "all"
+            output = output.select {|i| i.item_type == category}
+        end
+        if query != "" && query != nil
+            output = output.select do |i|
+                i.name.downcase.include?(query.downcase) || i.description.downcase.include?(query.downcase)
+            end
+        end
+        # byebug
+        output
     end
 
 end
